10bd0f2e51f4478a05e0fa92b2fc641f1eae5fa8
Lyuben Dimitrov <ldimitrov070@gmail.com>
2019-07-10 22:50:42 +0200

    added some dummy questions and substituted hardcoded values

--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_scrum_quiz/util/question.dart';
 import 'package:flutter_scrum_quiz/util/quiz.dart';
 import 'package:flutter_scrum_quiz/ui_elements/answer_button.dart';
 import 'package:flutter_scrum_quiz/ui_elements/question_text.dart';
+import 'package:flutter_scrum_quiz/ui_elements/answer_overlay.dart';
 
 class QuizPage extends StatefulWidget {
   @override
@@ -10,17 +11,42 @@ class QuizPage extends StatefulWidget {
 }
 
 class QuizPageState extends State<QuizPage> {
+  Question currentQuestion;
+  Quiz quiz = new Quiz([
+    new Question("Scrum is agile metodology", true),
+    new Question("All activities in Scrum have a timebox", true),
+    new Question("The product owner has the final word on the backlog", true),
+    new Question("Sprint planing must be done every week", false),
+    new Question("Sprint duration is always 2 weeks", false),
+    new Question("Scrum advocates for teams with more than 9 people", false),
+  ]);
+
+  String questionText;
+  int questionNumber;
+  bool isCorrect;
+  bool overlayIsVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    currentQuestion = quiz.nextQuestion;
+    questionNumber = quiz.questionNumber;
+    questionText = currentQuestion.question;
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Stack(
+      fit: StackFit.expand,
       children: <Widget>[
         new Column(
           children: <Widget>[
             new AnswerButton(true, () => print("correct")),
-            new QuestionText("Pizza is nice", 1),
+            new QuestionText(questionText, questionNumber),
             new AnswerButton(false, () => print("false")),
           ],
         ),
+        overlayIsVisible == true ? new AnswerOverlay(true) : new Container()
       ],
     );
   }
